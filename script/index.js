fcea58bf3ddf7756d5956a18a34b808508caab00
Arkeopix <mendiej750@gmail.com>
2014-09-28 23:33:10 +0200

    lol

--- script/index.js
+++ script/index.js
@@ -5,6 +5,7 @@ window.onload=function() {
 	var img = document.createElement( 'img' );
 	img.setAttribute( 'class', 'red_dot' );
 	img.setAttribute( 'src', 'assets/reddot.jpg' );
+	img.setAttribute( 'dragable', 'true' );
 	img.setAttribute( 'height', '50px' );
 	img.setAttribute( 'width', '50px' );
 	red.appendChild( img );
@@ -15,6 +16,7 @@ window.onload=function() {
 	var img = document.createElement( 'img' );
 	img.setAttribute( 'class', 'blue_dot' );
 	img.setAttribute( 'src', 'assets/150px-Blue_sphere.svg.png' );
+	img.setAttribute( 'dragable', 'true' );
 	img.setAttribute( 'height', '50px' );
 	img.setAttribute( 'width', '50px' );
 	blue.appendChild( img );
