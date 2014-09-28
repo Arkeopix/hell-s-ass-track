6faeaf3fc9ca124cd0c3ad1f3901c3e9c4d059fb
Arkeopix <mendiej750@gmail.com>
2014-09-28 23:15:11 +0200

    dots are good

--- script/index.js
+++ script/index.js
@@ -1,3 +1,22 @@
 window.onload=function() {
-    
+
+    var red = document.getElementById( 'red' );
+    for ( var i = 0; i < 10; i++ ) {
+	var img = document.createElement( 'img' );
+	img.setAttribute( 'class', 'red_dot' );
+	img.setAttribute( 'src', 'assets/reddot.jpg' );
+	img.setAttribute( 'height', '50px' );
+	img.setAttribute( 'width', '50px' );
+	red.appendChild( img );
+    }
+
+    var blue = document.getElementById( 'blue' );
+    for ( var i = 0; i < 10; i++ ) {
+	var img = document.createElement( 'img' );
+	img.setAttribute( 'class', 'blue_dot' );
+	img.setAttribute( 'src', 'assets/150px-Blue_sphere.svg.png' );
+	img.setAttribute( 'height', '50px' );
+	img.setAttribute( 'width', '50px' );
+	red.appendChild( img );
+    }
 }
