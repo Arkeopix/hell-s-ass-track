e3fa82f6b30bbaab021d85cb0709a6f1806144ad
Arkeopix <mendiej750@gmail.com>
2014-09-29 16:22:20 +0200

    fixPageXY is useless

--- script/index.js
+++ script/index.js
@@ -1,22 +1,10 @@
-function fixPageXY( e ) {
-    if ( e.pageX == null && e.clientX != null ) { 
-	var html = document.documentElement
-	var body = document.body
-	e.pageX = e.clientX + ( html.scrollLeft || body && body.scrollLeft || 0 )
-	e.pageX -= html.clientLeft || 0
-	e.pageY = e.clientY + ( html.scrollTop || body && body.scrollTop || 0 )
-	e.pageY -= html.clientTop || 0
-    }
-}
-
 function dragThisShit() {
     this.style.position = 'absolute';
     var self = this; 
     document.onmousemove = function( e ) {
 	e = e || event;
-	fixPageXY( e );
-	self.style.left = e.pageX-25+'px';
-	self.style.top = e.pageY-25+'px';
+	self.style.left = e.pageX - 25 + 'px';
+	self.style.top = e.pageY - 25 + 'px';
     }
     this.onmouseup = function() {
 	document.onmousemove = null;
