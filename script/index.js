3ca8758087f4a51a58dc6afa33f0cfb7973b5d9b
Arkeopix <mendiej750@gmail.com>
2014-09-29 14:45:59 +0200

    ffs there's always something wrong

--- script/index.js
+++ script/index.js
@@ -2,17 +2,33 @@ function fixPageXY( e ) {
     if ( e.pageX == null && e.clientX != null ) { 
 	var html = document.documentElement
 	var body = document.body
-
 	e.pageX = e.clientX + ( html.scrollLeft || body && body.scrollLeft || 0 )
 	e.pageX -= html.clientLeft || 0
-	
 	e.pageY = e.clientY + ( html.scrollTop || body && body.scrollTop || 0 )
 	e.pageY -= html.clientTop || 0
     }
 }
 
-function create_teams( color ) {
+function dragThisShit() {
+    this.style.position = 'absolute';
+    var self = this; 
+    document.onmousemove = function( e ) {
+	e = e || event;
+	fixPageXY( e );
+	self.style.left = e.pageX-25+'px';
+	self.style.top = e.pageY-25+'px';
+    }
+    this.onmouseup = function() {
+	document.onmousemove = null;
+    }
+}
 
+function resetPlayers( color ) {
+    console.log( document.getElementById( color ) );
+    document.getElementById( color ).classList.remove( 'drag_ok' );
+}
+
+function createTeam( color ) {
     var team = document.getElementById( color );
     for ( var i = 0; i < 10; i++ ) {
 	var img = document.createElement( 'img' );
@@ -21,27 +37,13 @@ function create_teams( color ) {
 	img.setAttribute( 'dragable', 'true' );
 	img.setAttribute( 'height', '50px' );
 	img.setAttribute( 'width', '50px' );
-	img.onmousedown = function() {
-	    this.style.position = 'absolute';
-	    
-	    var self = this;
-	    
-	    document.onmousemove = function( e ) {
-		e = e || event;
-		fixPageXY( e );
-		self.style.left = e.pageX-25+'px';
-		self.style.top = e.pageY-25+'px';
-	    }
-	    this.onmouseup = function() {
-		document.onmousemove = null;
-	    }
-	}
+	img.onmousedown = dragThisShit;
 	img.ondragstart = function() { return false }
 	team.appendChild( img );
     }
 }
 
 window.onload=function() {
-    create_teams( 'red' );
-    create_teams( 'blue' );
+    createTeam( 'red' );
+    createTeam( 'blue' );
 }
