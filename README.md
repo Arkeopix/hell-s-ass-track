880428ddecdcdf7140b27e0a5aa5b1ce4ae3cc33
Arkeopix <mendiej750@gmail.com>
2014-11-14 16:42:00 +0100

    new players

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 hell-s-ass-track
 ================
 
-This is a small webapp developed for the hell's ass derby girls team in Strasbourg. The purpose is to show and explain how roller derby works
+This is a small webapp developed for the hell's ass derby girls team in
+Strasbourg. The purpose is to show and explain how roller derby works
+
 
 ps: I'm no designer, it's ugly but it works.
--- script/index.js
+++ script/index.js
@@ -1,42 +1,49 @@
 function dragThisShit() {
-    this.style.position = 'absolute';
-    var self = this; 
-    document.onmousemove = function( e ) {
-	e = e || event;
-	self.style.left = e.pageX - 25 + 'px';
-	self.style.top = e.pageY - 25 + 'px';
-    }
-    this.onmouseup = function() {
-	document.onmousemove = null;
-    }
+  this.style.position = 'absolute';
+  var self = this;
+
+  document.onmousemove = function( e ) {
+	   e = e || event;
+	   self.style.left = e.pageX - 25 + 'px';
+	   self.style.top = e.pageY - 25 + 'px';
+  }
+  this.onmouseup = function() {
+	   document.onmousemove = null;
+  }
 }
 
 function resetPlayers( color ) {
-    console.log( document.getElementById( color ).classList );
-    for ( var i = 0; i < 10; i++ ) {
-	var to_del = document.getElementById( color + i );
-	to_del.parentNode.removeChild( to_del );
-    }
-    createTeam( color );
+  console.log( document.getElementById( color ).classList );
+  for ( var i = 0; i < 10; i++ ) {
+	   var to_del = document.getElementById( color + i );
+	    to_del.parentNode.removeChild( to_del );
+  }
+  createTeam( color );
 }
 
 function createTeam( color ) {
-    var team = document.getElementById( color );
-    for ( var i = 0; i < 10; i++ ) {
-	var img = document.createElement( 'img' );
-	img.setAttribute( 'class', 'drag_ok' );
-	img.setAttribute( 'id', color + i );
-	img.setAttribute( 'src', 'assets/' + color + 'dot.jpg' );
-	img.setAttribute( 'draggable', 'true' );
-	img.setAttribute( 'height', '35px' );
-	img.setAttribute( 'width', '35px' );
-	img.onmousedown = dragThisShit;
-	img.ondragstart = function() { return false }
-	team.appendChild( img );
-    }
+  var team = document.getElementById( color );
+
+  for ( var i = 0; i < 10; i++ ) {
+    var img = document.createElement( 'img' );
+	  img.setAttribute( 'class', 'drag_ok' );
+	  img.setAttribute( 'id', color + i );
+
+    var str_attribute = ( i == 0 ) ? 'assets/' + color + '_jammer.png'
+      : ( i == 1 ) ? 'assets/' + color + '_pivot.png'
+      : 'assets/' + color + '.png';
+
+    img.setAttribute( 'src', str_attribute );
+    img.setAttribute( 'draggable', 'true' );
+	  img.setAttribute( 'height', '35px' );
+    img.setAttribute( 'width', '35px' );
+    img.onmousedown = dragThisShit;
+    img.ondragstart = function() { return false }
+    team.appendChild( img );
+  }
 }
 
 window.onload=function() {
-    createTeam( 'red' );
-    createTeam( 'blue' );
+  createTeam( 'red' );
+  createTeam( 'blue' );
 }
